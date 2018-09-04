9b4087d354a3eddcf120bd7bfa062dae49b34acd
DESKTOP-C7ND9C0\proitzen <lakmal@proitzen.com>
2018-09-04 14:55:47 +0530

    init add

---
+++ app/src/main/java/com/udayalakmal/recyclerviewdragswipe/RecyclerGridFragment.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2015 Paul Burke
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.udayalakmal.recyclerviewdragswipe;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.udayalakmal.recyclerviewdragswipe.helper.OnCustomerListChangedListener;
+import com.udayalakmal.recyclerviewdragswipe.helper.OnStartDragListener;
+import com.udayalakmal.recyclerviewdragswipe.helper.SimpleItemTouchHelperCallback;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Udaya Lakmal Rathnasiri
+ */
+public class RecyclerGridFragment extends Fragment implements OnStartDragListener {
+
+    private ItemTouchHelper mItemTouchHelper;
+    RecyclerListAdapter adapter;
+
+    public RecyclerGridFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return new RecyclerView(container.getContext());
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("One");
+        list.add("Two");
+        list.add("Three");
+        list.add("Four");
+        list.add("Five");
+        list.add("Six");
+        list.add("Seven");
+        list.add("Eight");
+        list.add("Nine");
+        list.add("Ten");
+
+
+        adapter = new RecyclerListAdapter(getActivity(), this, list, new OnCustomerListChangedListener() {
+            @Override
+            public void onNoteListChanged(List<String> changedList) {
+                ArrayList<String> mItems = (ArrayList<String>) changedList;
+                System.out.println("final Item rearanged");
+
+                for (int i = 0; i < mItems.size(); i++) {
+                    System.out.print(" " + mItems.get(i));
+                }
+                System.out.println(" \n-----");
+            }
+        });
+
+        RecyclerView recyclerView = (RecyclerView) view;
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
+
+        final int spanCount = 4;
+        final GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), spanCount);
+        recyclerView.setLayoutManager(layoutManager);
+
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(adapter);
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(recyclerView);
+    }
+
+    @Override
+    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
+        mItemTouchHelper.startDrag(viewHolder);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+}
---
+++ app/src/main/java/com/udayalakmal/recyclerviewdragswipe/helper/OnCustomerListChangedListener.java
@@ -0,0 +1,8 @@
+package com.udayalakmal.recyclerviewdragswipe.helper;
+
+import java.util.List;
+
+public interface OnCustomerListChangedListener {
+        void onNoteListChanged(List<String> customers);
+}
+
